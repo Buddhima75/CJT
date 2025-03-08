7d5b11a5756984eeb8cd286b73de867188411bf8
Buddhima75 <buddima60@gmail.com>
2025-03-08 22:53:30 +0530

    up

---
+++ header.php
@@ -0,0 +1,209 @@
+<header>
+    <div class="top-navigation">
+        <div class="logo-section">
+            <a href="http://localhost/cjd" id="branding" title="logo-black"><img src="./images/logo/logo.png" alt="logo-black"
+                    title="logo-black"></a> 
+                    <div class="img-logo-top" >
+                <img src="./images/logo/logo.png"  alt="truly sri lanka" title="truly sri lanka">
+            </div>
+            
+        </div>
+        <div  class="special-links-section">
+            <ul id="menu-top-menu" class="">
+                
+                <li><a href="./destination.php"><span>Explore</span></a></li>
+                
+                <li><a href="http://localhost/cjd/things-to-do.html"><span>Things to Do</span></a></li>
+                <li><a href="http://localhost/cjd/accommodation/"><span>Accommodation</span></a></li>
+                <li><a href="http://localhost/cjd/leisure-recreations/"><span>leisure & Recreations</span></a></li>
+                <li><a href="http://localhost/cjd/transportation-vehicles/"><span>Transportation & Vehicles</span></a></li>
+            </ul>
+        </div>
+
+
+        <!-- /.menu-section -->
+        <div class="plan-your-gateway-section">
+            <div class="menu-section">
+                <div class="menu-toggle mainmenutoggle">
+                    <div class="nav-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+
+                <div class="main-menu-container clearfix">
+                    <div class="table-view">
+
+
+                        <div class="logo-menu-img">
+                            <!-- logo coulumn start -->
+                            <div class="column-view logo-column">
+                                <div class="column-inner-wrapper">
+                                    <a href="http://localhost/cjd" class="logo-black-desktop clearfix"
+                                        title="logo-black">
+                                        <img src="./images/logo/logo.png" alt="logo-black" title="logo-black">
+                                    </a>
+
+
+                                    <!-- 						<div class="txt-wrapper">
+							"Discover your next adventure on the isle of serendip.."
+						</div>
+					 -->
+
+
+                                    <div class="desktop-menu-social-media-section">
+                                        <ul>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <!-- /.column-inner-wrapper -->
+                            </div>
+                            <!-- logo coulumn end -->
+
+                            <!-- menu-area start-->
+                            <div class="column-view menu-column clearfix">
+                                <div class="main-menu-social-media-mobile-container">
+                                    <div class="main-menu-mobile-container">
+                                        <ul id="main-menu" class="">
+                                            <li class="current-menu-item"><a href="http://localhost/cjd/"
+                                                    aria-current="page">Home</a></li>
+                                            <li><a href="./destination.php">Explore</a>
+                                            </li>
+                                            <li><a href="http://localhost/cjd/things-to-do.html">Things to
+                                                    Do</a></li>
+                                                    <li><a href="http://localhost/cjd/accommodation/"><span>Accommodation</span></a></li>
+                                                    <li><a href="http://localhost/cjd/leisure-recreations/"><span>leisure & Recreations</span></a></li>
+                                                    <li><a href="http://localhost/cjd/transportation-vehicles/"><span>Transportation & Vehicles</span></a></li>
+                                                    <li><a href="./about_us.php">About Us</a></li>
+                                                    <li><a href="./contact_us.php">Contact Us</a>
+                                                      
+                                            
+                                        </ul>
+                                    </div>
+
+                                    <!-- /#main-menu -->
+
+                                    <div class="mobile-social-media-section">
+                                        <ul>
+                                            <li>
+                                                <a href="https://www.facebook.com/VisitTrulySriLanka/" target="_blank">
+                                                    <svg class="icon svg-fb">
+                                                        <use xlink:href="#svg-icon-fb"></use>
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="https://www.instagram.com/sl/" target="_blank">
+                                                    <svg class="icon svg-instagram">
+                                                        <use xlink:href="#svg-icon-instagram"></use>
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="https://twitter.com/sri_truly" target="_blank">
+                                                    <svg class="icon svg-twitter">
+                                                        <use xlink:href="#svg-icon-twitter"></use>
+                                                    </svg>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <!-- /.main-menu-mobile-container --->
+                                <div class="main-menu-desktop-container">
+                                    <ul id="main-menu-desktop" class="">
+                                        <li class="current-menu-item"><a href="http://localhost/cjd/"
+                                                aria-current="page">Home</a></li>
+                                        <li><a href="http://localhost/cjd/destinations.html">Explore</a>
+                                        </li>
+                                        <li><a href="http://localhost/cjd/things-to-do.html">Things to
+                                                Do</a>
+                                        </li>
+                                        <li><a href="http://localhost/cjd/accommodation/"><span>Accommodation</span></a></li>
+                                        <li><a href="http://localhost/cjd/leisure-recreations/"><span>leisure & Recreations</span></a></li>
+                                        <li><a href="http://localhost/cjd/transportation-vehicles/"><span>Transportation & Vehicles</span></a></li>
+                                        <li><a href="./about_us.php">About Us</a></li>
+                                        <li><a href="./contact_us.php">Contact Us</a>
+                                        </li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                            <!-- /.column-view -->
+                            <!-- menu-area close-->
+
+
+                            <div class="mega-iamge" style="background-image: url(./images/39.jpg);">
+                            </div>
+                        </div>
+
+                        <!-- featured-destin start -->
+                        <div class="column-view featured-column">
+                            <div class="featured-items-wrapper">
+                                <div class="line-title"> Featured destinations </div>
+
+                                <!-- <ul>
+						
+							<li>
+								<a href="http://localhost/cjd/destinations/colombo.html">
+									Colombo								</a>
+							</li>
+
+						
+							<li>
+								<a href="http://localhost/cjd/destinations/galle.html">
+									Galle								</a>
+							</li>
+
+						
+							<li>
+								<a href="http://localhost/cjd/destinations/kandy.html">
+									Kandy								</a>
+							</li>
+
+											</ul> -->
+
+
+                                <div class="owl-carousel feat-destin">
+                                    <div class="item">
+                                        <a href="http://localhost/cjd/destinations/colombo.html"
+                                            style="background-image: url(./images/colombo.jpg);">
+                                            <span>Colombo</span>
+                                        </a>
+                                    </div>
+                                    <div class="item">
+                                        <a href="http://localhost/cjd/destinations/galle.html"
+                                            style="background-image: url(./images/Kandy.jpg);">
+                                            <span>Kandy</span>
+                                        </a>
+                                    </div>
+                                    <div class="item">
+                                        <a href="http://localhost/cjd/destinations/kandy.html"
+                                            style="background-image: url(./images/4.jpg);">
+                                            <span>Ella</span>
+                                        </a>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <!-- /.column-view -->
+                        <!-- featured-destin end -->
+
+                    </div>
+                    <!-- /.table-view -->
+                </div>
+
+                <!-- /.main-menu-container -->
+            </div>
+
+            <!-- /.plan-your-gateway-container -->
+        </div>
+        <!-- /.plan-your-gateway-section -->
+    </div>
+
+    <!-- /.top-navigation -->
+</header>
